b55668f1387809628ea44857fc5fb51950004b4b
Guilherme Lino <guilhermelinosp@outlook.com>
2024-11-01 07:18:34 -0300

    up

--- configs/errs/err.go
+++ configs/errs/err.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// Error represents an error response.
 type Error struct {
 	Message string   `json:"message"`
 	ErrType string   `json:"error"`
@@ -12,6 +13,7 @@ type Error struct {
 	Causes  []*Cause `json:"causes,omitempty"`
 }
 
+// Cause represents a cause of an error.
 type Cause struct {
 	Field   string `json:"field"`
 	Message string `json:"message"`
@@ -27,30 +29,37 @@ func NewError(message, error string, code int, causes ...*Cause) *Error {
 	}
 }
 
+// NewValidationError creates a new validation error instance.
 func NewValidationError(message string, causes []*Cause) *Error {
 	return NewError(message, "validation_error", http.StatusBadRequest, causes...)
 }
 
+// NewBadRequest creates a new bad request error instance.
 func NewBadRequest(message string) *Error {
 	return NewError(message, "bad_request", http.StatusBadRequest)
 }
 
+// NewNotFound creates a new not found error instance.
 func NewNotFound(message string) *Error {
 	return NewError(message, "not_found", http.StatusNotFound)
 }
 
+// NewInternalServerError creates a new internal server error instance.
 func NewInternalServerError(message string) *Error {
 	return NewError(message, "internal_server_error", http.StatusInternalServerError)
 }
 
+// NewUnauthorized creates a new unauthorized error instance.
 func NewUnauthorized(message string) *Error {
 	return NewError(message, "unauthorized", http.StatusUnauthorized)
 }
 
+// NewConflict creates a new conflict error instance.
 func NewConflict(message string) *Error {
 	return NewError(message, "conflict", http.StatusConflict)
 }
 
+// ToJSON converts the error instance to a JSON string.
 func (r *Error) ToJSON() string {
 	jsonData, err := json.Marshal(r)
 	if err != nil {
@@ -59,11 +68,12 @@ func (r *Error) ToJSON() string {
 	return string(jsonData)
 }
 
+// AddCause appends a new cause to the error instance.
 func (r *Error) AddCause(field, message string) {
 	r.Causes = append(r.Causes, &Cause{Field: field, Message: message})
 }
 
-// IsType checks if the error is of a specific type
+// IsType checks if the error type matches the provided type.
 func (r *Error) IsType(errType string) bool {
 	return r.ErrType == errType
 }
